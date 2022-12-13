fb0cb5e6f86ea03dd64d052cce836491a5c6d75d
dannycahyo <dannydwicahyono@gmail.com>
2022-12-13 09:08:52 +0700

    feat: implement search recipient

--- src/component/RecipientList.tsx
+++ src/component/RecipientList.tsx
@@ -1,14 +1,150 @@
+import React from "react";
 import useRecipient from "../hooks/useRecipient";
+import { BsSearch } from "react-icons/bs";
 
 const RecipientList = () => {
   const { recipients } = useRecipient();
+  const [searchRecipient, setSearchRecipient] = React.useState<string>("");
 
-  console.log(recipients, "TEST");
+  const filteredRecipients = recipients?.filter(
+    (recipient) =>
+      recipient.name.toLowerCase().includes(searchRecipient.toLowerCase()) ||
+      recipient.description
+        .toLowerCase()
+        .includes(searchRecipient.toLowerCase())
+  );
+
   return (
     <div className="px-4 sm:px-0">
       <h3 className="text-lg font-medium leading-6 text-gray-900">
         List Recipients
       </h3>
+
+      <div className="mt-2 flex justify-between items-center">
+        <div>
+          <div className="absolute mt-2 ml-2">
+            <BsSearch color="gray" />
+          </div>
+          <input
+            value={searchRecipient}
+            onChange={(e) => setSearchRecipient(e.target.value)}
+            type="text"
+            placeholder="Search recipient"
+            className="block w-full h-8 flex-1 rounded-l-md rounded-r-md border-gray-200 border-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm pl-8"
+          />
+        </div>
+
+        <div className="pr-6">
+          <button className="inline-flex justify-center rounded-md border border-transparent bg-blue-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 mr-2">
+            Undo
+          </button>
+          <button className="inline-flex justify-center rounded-md border border-transparent bg-blue-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2">
+            Redo
+          </button>
+        </div>
+      </div>
+
+      <div className="pt-4">
+        <div className="flex flex-col">
+          <div className="overflow-x-auto">
+            <div className="p-1.5 w-full inline-block align-middle">
+              <div className="overflow-hidden border rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="py-3 pl-4">
+                        <div className="flex items-center h-5">
+                          <input
+                            id="checkbox-all"
+                            type="checkbox"
+                            className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"
+                          />
+                          <label htmlFor="checkbox" className="sr-only">
+                            Checkbox
+                          </label>
+                        </div>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                      >
+                        Description
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                      >
+                        Amount
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                      >
+                        Tax
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                      >
+                        Discount
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                      >
+                        Total
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200">
+                    {filteredRecipients?.map((recipient) => (
+                      <tr key={recipient.id}>
+                        <td className="py-3 pl-4">
+                          <div className="flex items-center h-5">
+                            <input
+                              type="checkbox"
+                              className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"
+                            />
+                            <label htmlFor="checkbox" className="sr-only">
+                              Checkbox
+                            </label>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {recipient.name}
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {recipient.description}
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {recipient.amount}
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {recipient.tax !== 0 ? `${recipient.tax} %` : "-"}
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {recipient.discount !== 0
+                            ? `${recipient.discount} %`
+                            : "-"}
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {recipient.total}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
